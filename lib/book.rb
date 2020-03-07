44c163c770526b542543ee9417eea3dc5a364bd7
Brandon Dusch <brandondusch@gmail.com>
2020-03-07 17:41:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,10 @@
 class Book 
-  def initialize(title, author, page_count)
+  def initialize(title)
     @title = title
-    @author = author
-    @page_count = page_count
   end 
   
   def title 
     @title
-  end
-  
-  def author 
-    @author
-  end
-  
-  def page_count 
-    @page_count
   end
 end
 
